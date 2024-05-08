cf4e5a4919c1c2842c7bc4b07ec50a0c2ff3239b
VIROS <jhardie@student.neumont.edu>
2024-05-08 08:49:58 -0600

    Ticket Service API
    Fully functional, not yet set up with Eureka. Possibly auth requirements later?

---
+++ Ticket/backend/PRO290TicketServiceAPI/src/test/java/com/example/PRO290TicketServiceAPI/Pro290TicketServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PRO290TicketServiceAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pro290TicketServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
