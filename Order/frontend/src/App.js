1cbcf983303650e046a180027803dc06d41c0919
MooonSeer <129355115+MooonSeer@users.noreply.github.com>
2024-05-15 09:16:00 -0600

    More changes

--- Order/frontend/src/App.js
+++ Order/frontend/src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <body>
+      <header>
+        <nav>
+            <ul>
+                <li id="list1"><a href="home.html" style={{color: "#f3e3e2"}}>Home</a></li>
+                <li id="list2"><a href="App.js" style={{color: "#f3e3e2"}}>Inventory</a></li>
+                <li id="list3"><a href="deals.html" style={{color: "#f3e3e2"}}>Chat</a></li>
+                <li id="list4" style={{float: "right"}}><a href="profile.html" style={{color: "#f3e3e2"}}>Profile</a></li>
+                <li id="list5" style={{float: "right"}}><a href="C:\Users\kalawson\Documents\GitHub\pro290-s2-project\Cart\frontend\src\App.js" style={{color: "#f3e3e2"}}>Cart</a></li>
+            </ul>
+        </nav>
       </header>
-    </div>
+      <main>
+        <input type="text" style={{width: "30%", height: "30px"}} id="name" placeholder='Name on Card'/>
+        <br/>
+        <input type="text" style={{width: "30%", height: "30px"}} id="number" placeholder='Card Number'/>
+        <br/>
+        <input type="text" style={{width: "14.8%", height: "30px"}} id="cvv" placeholder='CVV'/>
+        <input type="text" style={{width: "14.8%", height: "30px"}} id="expiration" placeholder='Expiration'/>
+        <br/>
+        <input type="text" style={{width: "30%", height: "30px"}} id="address1" placeholder='Address Line 1'/>
+        <br/>
+        <input type="text" style={{width: "30%", height: "30px"}} id="address2" placeholder='Address Line 2'/>
+        <br/>
+        <input type="text" style={{width: "30%", height: "30px"}} id="zipcode" placeholder='Zip Code'/>
+      </main>
+    </body>
   );
 }
 
--- Ticket/frontend/src/App.js
+++ Ticket/frontend/src/App.js
@@ -18,20 +18,36 @@ function App() {
       <div class="split left">
           <div class="ticket" id="ticket1">
             <h2>Ticket 1</h2>
-            <h4>[status]</h4>
             <h4>[insert issue here]</h4>
+            <h4>[status]</h4>
           </div>
           <br/>
           <div class="ticket" id="ticket2">
             <h2>Ticket 2</h2>
-            <h4>[status]</h4>
             <h4>[insert issue here]</h4>
+            <h4>[status]</h4>
           </div>
       </div>
       <div class="split right">
         <div class="centered">
-          <h2>Ticket Details</h2>
-          <p>Select a ticket to view details.</p>
+          <div class="ticketDesc" id="desc1">
+            <h1>[Issue Name]</h1>
+            <h2>[status]</h2>
+            <h2>[date]</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur 
+              adipiscing elit. Cras a ultrices lacus. 
+              Sed vel tortor ac ex luctus pretium. 
+              Suspendisse bibendum, diam a venenatis 
+              rutrum, libero mauris auctor est, id malesuada 
+              mi orci vel ipsum. Vivamus quis nisi eleifend, 
+              sodales sapien a, ullamcorper nisi. Phasellus id 
+              sapien sapien. Maecenas rutrum arcu ac enim dapibus, 
+              in gravida diam egestas. Nulla facilisi. Etiam ipsum 
+              nunc, accumsan ut orci non, venenatis pharetra nisi. 
+              Donec sit amet pharetra odio. Aenean dapibus venenatis 
+              massa vitae imperdiet. Curabitur vel eros quis urna 
+              consectetur ultricies a at lorem. Aenean non ligula nibh.</p>
+          </div>
         </div>
       </div>
     </main>
