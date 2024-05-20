de41e4df7fd266d43bc19fbe7c1dddad1bbae6bd
VIROS <jhardie@student.neumont.edu>
2024-05-20 10:09:04 -0600

    API Gateway
    works, but does not yet contain ALL of the routing. Just ticket and mailing

---
+++ Mailing/PRO290APIGateway/src/main/java/org/example/pro290apigateway/Pro290ApiGatewayApplication.java
@@ -0,0 +1,30 @@
+package org.example.pro290apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class Pro290ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pro290ApiGatewayApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator customRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("ticketService", r -> r.path("/ticketService/**")
+                        .filters(f -> f.stripPrefix(1))
+                        .uri("lb://PRO290TICKETSERVICEAPI"))
+                .route("mailService", r -> r.path("/mailService/**")
+                        .filters(f -> f.stripPrefix(1))
+                        .uri("lb://PRO290MAILINGSERVICEAPI"))
+                .build();
+    }
+
+}
---
+++ Mailing/PRO290APIGateway/src/test/java/org/example/pro290apigateway/Pro290ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.pro290apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pro290ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
