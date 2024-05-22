fa84d573fd6586febfd55c35bc7d8543e5351f28
MooonSeer <129355115+MooonSeer@users.noreply.github.com>
2024-05-22 09:46:14 -0600

    Last frontend Commit
    All last changes made before merge to main

--- Home/src/App.js
+++ Home/src/App.js
@@ -7,14 +7,31 @@ function App() {
       <header>
         <nav>
             <ul>
-            <li id="list1"><a href="http://localhost:3000/" style={{color: "#f3e3e2"}}>Home</a></li>
+                <li id="list1"><a href="http://localhost:3000/" style={{color: "#f3e3e2"}}>Home</a></li>
                 <li id="list2"><a href="http://localhost:3001/" style={{color: "#f3e3e2"}}>Inventory</a></li>
                 <li id="list3"><a href="http://localhost:3004/" style={{color: "#f3e3e2"}}>Chat</a></li>
-                <li id="list4" style={{float: "right"}}><a href="http://localhost:3006/" style={{color: "#f3e3e2"}}>Profile</a></li>
-                <li id="list5" style={{float: "right"}}><a href="http://localhost:3002/" style={{color: "#f3e3e2"}}>Cart</a></li>
+                <li id="list4"><a href="http://localhost:3005/" style={{color: "#f3e3e2"}}>Ticket</a></li>
+                <li id="list5" style={{float: "right"}}><a href="http://localhost:3006/" style={{color: "#f3e3e2"}}>Profile</a></li>
+                <li id="list6" style={{float: "right"}}><a href="http://localhost:3002/" style={{color: "#f3e3e2"}}>Cart</a></li>
             </ul>
         </nav>
       </header>
+      <main>
+        <div class="split left">
+          <h1>Home</h1>
+          <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eleifend viverra varius. Integer efficitur neque id libero lacinia 
+          fermentum. Nullam pulvinar suscipit neque sit amet eleifend. Donec sit amet pellentesque odio, nec lacinia arcu. Etiam dapibus 
+          accumsan dui, eget varius metus sodales quis. Curabitur lobortis sit amet metus sed sodales. Donec lacinia, nisi nec aliquam commodo, 
+          massa quam facilisis tellus, a interdum risus nunc at urna. Mauris rutrum magna in volutpat mollis.
+
+          Aliquam vehicula sed urna at rutrum. Vestibulum in risus congue leo congue placerat nec sit amet leo. Ut id lorem id est maximus tempor. 
+          Quisque facilisis libero erat. Sed porta nisl eget interdum vehicula. Nullam egestas metus eu orci tristique ornare. Phasellus augue libero, 
+          faucibus vel blandit quis, sagittis ut enim. Quisque neque augue, varius a leo sit amet, gravida fermentum tortor. Nunc tristique leo sed bibendum tempus.
+          </p>
+        </div>
+        
+      </main>
     </body>
   );
 }
--- Ticket/frontend/src/App.js
+++ Ticket/frontend/src/App.js
@@ -6,22 +6,24 @@ function App() {
       <header>
       <nav>
           <ul>
-              <li id="list1"><a href="home.html" style={{color: "#f3e3e2"}}>Home</a></li>
-              <li id="list2"><a href="http://localhost:3001" style={{color: "#f3e3e2"}}>Inventory</a></li>
-              <li id="list3"><a href="deals.html" style={{color: "#f3e3e2"}}>Chat</a></li>
-              <li id="list4" style={{float: "right"}}><a href="profile.html" style={{color: "#f3e3e2"}}>Profile</a></li>
-              <li id="list5" style={{float: "right"}}><a href="C:\Users\kalawson\Documents\GitHub\pro290-s2-project\Cart\frontend\src\App.js" style={{color: "#f3e3e2"}}>Cart</a></li>
+              <li id="list1"><a href="http://localhost:3000/" style={{color: "#f3e3e2"}}>Home</a></li>
+              <li id="list2"><a href="http://localhost:3001/" style={{color: "#f3e3e2"}}>Inventory</a></li>
+              <li id="list3"><a href="http://localhost:3004/" style={{color: "#f3e3e2"}}>Chat</a></li>
+              <li id="list4"><a href="http://localhost:3005/" style={{color: "#f3e3e2"}}>Ticket</a></li>
+              <li id="list5" style={{float: "right"}}><a href="http://localhost:3006/" style={{color: "#f3e3e2"}}>Profile</a></li>
+              <li id="list6" style={{float: "right"}}><a href="http://localhost:3002/" style={{color: "#f3e3e2"}}>Cart</a></li>
           </ul>
       </nav>
     </header>
     <main>
       <div class="split left">
+        <button id="create">Create Ticket</button>
+        <br />
           <div class="ticket" id="ticket1">
             <h2>Ticket 1</h2>
             <h4>[insert issue here]</h4>
             <h4>[status]</h4>
           </div>
-          <br/>
           <div class="ticket" id="ticket2">
             <h2>Ticket 2</h2>
             <h4>[insert issue here]</h4>
